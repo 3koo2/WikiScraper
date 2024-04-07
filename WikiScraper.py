b65ac14e5d1f8e077479fb35b2f3e95f22adaa49
3koo2 <egravedp@gmail.com>
2024-04-07 14:53:21 -0400

    added stuff

--- WikiScraper.py
+++ WikiScraper.py
@@ -1,1 +1,14 @@
 #WikiScraper - Scrape wikipedia
+import requests #request thingy
+from bs4 import BeautifulSoup #html parser
+
+#First, we need to know which pages to scrape
+pagesstring = input("Enter the titles of the pages to scrape (separate with tabs):\n")
+pages = pagesstring.split("\t")
+
+#Next, we loop through the pages array
+for page in pages:
+    #form the full url
+    url = "http://wikipedia.org/wiki/"+page
+    request = requests.get(url, {"user-agent":"frgo-admin@frgo.org|github.com/3koo2"})
+    print(request.content)
